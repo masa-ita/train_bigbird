faf43a2315573e31725482c64e9640b505a9ce66
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-11 12:44:28 +0900

    add unk_id and pad_id options

--- train_sentencepiece.sh
+++ train_sentencepiece.sh
@@ -6,5 +6,7 @@
   --character_coverage=0.9995 \
   --model_type=bpe \
   --max_sentence_length=16384 \
+  --unk_id=3 \
+  --pad_id=0 \
   --control_symbols="[CLS],[SEP],[MASK]" \
   --user_defined_symbols="_START_ARTICLE_,_START_SECTION_,_START_PARAGRAPH_,_NEWLINE_"
