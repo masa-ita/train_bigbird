632ec7a4b0005b96093e3e857a85f10392ef54bb
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-14 10:21:40 +0900

    fix flags

--- pretrain_bigbird.sh
+++ pretrain_bigbird.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 python3 run_pretraining.py \
-    --data_dir gs://${STORAGE_BUCKET}/data/ \
+    --data_dir ${STORAGE_BUCKET}/data/ \
     --output_dir ${MODEL_DIR} \
     --preprocessed_data \
+    --max_encoder_length 4096 \
     --max_predictions_per_seq 20 \
     --do_train \
     --do_eval \
@@ -15,7 +16,7 @@ python3 run_pretraining.py \
     --num_warmup_steps 10000 \
     --save_checkpoints_steps 50000 \
     --max_eval_steps 1000 \
-    --vocab_model_file ${MODEL_DIR}/sp_wiki40b_32k.model \
+    --vocab_model_file ${MODEL_DIR}/spm_BPE_32K.model \
     --use_nsp \
     --use_tpu \
     --tpu_name="$TPU_NAME" \
