ac3e00a5c60917f0b99586fb747e3bd362a8352f
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-15 16:49:01 +0900

    move argparse out of main()

--- extract_sentences.py
+++ extract_sentences.py
@@ -14,15 +14,7 @@ def break_lines(text):
     text = re.sub(r'^\n','', text)
     return(text)
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--split", type=str, choices=['train', 'validation', 'test'], 
-                        help="dataset split: train, validation, test", default='train')
-    parser.add_argument("--output_file", type=str, help="output file contains sentences", required=True)
-    parser.add_argument("--cache_dir", type=str, help="tensorflow datasets cache directory")
-
-    args = parser.parse_args()
-    
+def main(args):    
     dataset = tfds.load("wiki40b/ja", data_dir=args.cache_dir) 
     with open(args.output_file, 'w') as f:
         for data in tqdm(dataset[args.split]):
@@ -30,4 +22,12 @@ def main():
             f.write(break_lines(text))
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--split", type=str, choices=['train', 'validation', 'test'], 
+                        help="dataset split: train, validation, test", default='train')
+    parser.add_argument("--output_file", type=str, help="output file contains sentences", required=True)
+    parser.add_argument("--cache_dir", type=str, help="tensorflow datasets cache directory")
+
+    args = parser.parse_args()
+
+    main(args)
