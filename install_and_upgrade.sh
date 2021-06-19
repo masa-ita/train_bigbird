299d28bc7d28de7c1265994534016390b073b2d9
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-19 09:22:00 +0900

    add tensorboard-plugin-profile to be installed.

--- install_and_upgrade.sh
+++ install_and_upgrade.sh
@@ -1,4 +1,4 @@
-pip3 install --upgrade "cloud-tpu-profiler>=2.4.0"
+pip3 install --upgrade "cloud-tpu-profiler>=2.4.0" "tensorboard-plugin-profile"
 sudo pip3 install --upgrade -U "tensorboard>=2.5"
 sudo pip3 install --upgrade -U "tensorflow>=2.5"
 cd bigbird && pip3 install -e .
