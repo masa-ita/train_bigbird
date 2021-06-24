d09fc931cb1ce254796721d306b2e9e82e1b6148
Masatoshi Itagaki <masatoshi@rails.to>
2021-06-24 14:17:45 +0900

    add create_tfrecord_files.sh

---
+++ create_tfrecord_files.sh
@@ -0,0 +1,17 @@
+for FILE in $1/*; do
+BASENAME=${FILE##*/}
+EXT=${FILE%.*}
+STEM=${BASENAME%.*}
+python3 src/create_pretraining_data.py \
+    --input_file=$f \
+    --output_file=$2/$STEM.tfrecords-$EXT \
+    --model_file=$3.model \
+    --vocab_file=$3.vocab \
+    --do_lower_case=True \
+    --max_seq_length=4096 \
+    --max_predictions_per_seq=615 \
+    --masked_lm_prob=0.15 \
+    --random_seed=0 \
+    --dupe_factor=10 \
+    --do_whole_word_mask=False &
+done
