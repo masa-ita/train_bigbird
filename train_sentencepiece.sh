7872ed44cfa669ce9e42d071e3caef78cb9ff642
Masatoshi Itagaki <masatoshi@rails.to>
2021-07-15 12:22:59 +0900

    enable to select model type in train_sentencepiece.sh

--- train_sentencepiece.sh
+++ train_sentencepiece.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [ $# -ne 2 ]; then
-  echo "Usage: train_sentencepiece.sh <input_file> <model_prefix>" 1>&2
+  echo "Usage: train_sentencepiece.sh <input_file> <model_type> <model_prefix>" 1>&2
   exit 1
 fi
 
 /usr/local/bin/spm_train \
   --input=$1 \
-  --model_prefix=$2 \
+  --model_prefix=$3 \
   --vocab_size=32000 \
   --character_coverage=0.9995 \
-  --model_type=bpe \
+  --model_type=$2 \
   --max_sentence_length=16384 \
   --unk_id=3 \
   --pad_id=0 \
